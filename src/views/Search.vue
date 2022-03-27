16595feb4f614654618e6d0d17fd3931b99a763c
Garavirod <rodrigogarciaavila26@gmail.com>
2022-03-27 11:47:01 -0600

    onActivated

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,34 +1,45 @@
 <template>
-    <div class="container fluid">
-        <h1>Search pokem : {{pokemonid}} </h1>
-        <form @submit.prevent="onSubmit">
-            <input
-            v-model="pokemonid"
-            class="form-control" 
-            placeholder="Number pokemon"
-            autofocus
-            type="number">
-        </form>
-    </div>
+  <div class="container fluid">
+    <h1>Search pokem : {{ pokemonid }}</h1>
+    <form @submit.prevent="onSubmit">
+      <input
+        ref="InputID"
+        v-model="pokemonid"
+        class="form-control"
+        placeholder="Number pokemon"
+        type="number"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { useRouter } from 'vue-router'
+import { ref, onActivated } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-    name:'Search',
-    setup(){
-        const router = useRouter()
-        const pokemonid = ref(1)
-        const onSubmit = () => {
-            console.log(pokemonid.value);
-            router.push({name:'pokemon-detail',params:{id:pokemonid.value}})
-        }
-        return{
-            pokemonid,
-            onSubmit
-        }
-    }
-}
+  name: "Search",
+  setup() {
+    const router = useRouter();
+    const pokemonid = ref(1);
+    const InputID = ref();
+
+    // Local
+    const onSubmit = () => {
+      //console.log(pokemonid.value);
+      router.push({ name: "pokemon-detail", params: { id: pokemonid.value } });
+    };
+
+    //Life cycle
+    onActivated(() => {
+      InputID.value.select();
+    });
+
+    return {
+      pokemonid,
+      onSubmit,
+      InputID,
+    };
+  },
+};
 </script>
